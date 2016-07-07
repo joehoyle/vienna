52610e63725c291db66077c2f57359a14ed9145f
Joe Hoyle <joehoyle@gmail.com>
2016-07-07 11:51:25 +0200

    Set the active site when the site route switches to another site

--- reducers/activeSite.js
+++ reducers/activeSite.js
@@ -2,6 +2,11 @@ export default function activeSite( state = { id: null }, action ) {
 	switch ( action.type ) {
 		case 'ADD_SITE_START':
 			return {...state, id: action.id }
+		case 'ROUTER_PUSH':
+			if ( action.payload.name === 'site' ) {
+				return {...state, id: action.payload.data.siteId}
+			}
+			break
 		case 'ADD_SITE_FAILED':
 		case 'SITE_REMOVED':
 			return {...state, id: null }
