f1a8c877401660a1b1187895ba2dc8b92de97f8b
Joe Hoyle <joehoyle@gmail.com>
2017-05-15 22:08:27 +0900

    Make all actions use .then*(

--- actions/fetchSettings.js
+++ actions/fetchSettings.js
@@ -1,22 +1,22 @@
-import httpapi from '../api'
+import httpapi from '../api';
 
-export default function fetchSettings( args ) {
-	return ( dispatch, getStore ) => {
-		const store = getStore()
-		const api = new httpapi( store.sites[ store.activeSite.id ] )
+export default function fetchSettings(args) {
+	return (dispatch, getStore) => {
+		const store = getStore();
+		const api = new httpapi(store.sites[store.activeSite.id]);
 
-		args = { context: 'edit', ...args }
+		args = { context: 'edit', ...args };
 
 		dispatch({
 			type: 'SETTINGS_UPDATING',
-		})
-		api.get( '/wp/v2/settings', args, function( data, err ) {
+		});
+		api.get('/wp/v2/settings', args).then(function(data, err) {
 			dispatch({
 				type: 'SETTINGS_UPDATED',
 				payload: {
-					settings: data
-				}
-			})
-		})
-	}
+					settings: data,
+				},
+			});
+		});
+	};
 }
--- actions/trashComment.js
+++ actions/trashComment.js
@@ -1,18 +1,18 @@
-import httpapi from '../api'
+import httpapi from '../api';
 
-export default function trashComment( commentId ) {
-	return ( dispatch, getStore ) => {
-		const store = getStore()
-		const api = new httpapi( store.sites[ store.activeSite.id ] )
+export default function trashComment(commentId) {
+	return (dispatch, getStore) => {
+		const store = getStore();
+		const api = new httpapi(store.sites[store.activeSite.id]);
 
 		dispatch({
 			type: 'COMMENT_TRASHING',
-		})
-		api._delete( '/wp/v2/comments/' + commentId, {}, function( data, err ) {
+		});
+		api._delete('/wp/v2/comments/' + commentId, {}).then(function(data, err) {
 			dispatch({
 				type: 'COMMENT_TRASHED',
-				payload: { commentId }
-			})
-		})
-	}
+				payload: { commentId },
+			});
+		});
+	};
 }
