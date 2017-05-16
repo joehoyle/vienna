5db26f1c6ff5df77c92772370b642999e4ed938a
Ryan McCue <me@ryanmccue.info>
2017-05-16 17:19:59 +1000

    Add delegates and attachment handlers

--- ios/Vienna/EditorManager.swift
+++ ios/Vienna/EditorManager.swift
@@ -17,12 +17,58 @@ class EditorManager: RCTViewManager {
 		let font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
 		let missingImage = Gridicon.iconOfType(.image)
 		let textView = Aztec.TextView(defaultFont: font, defaultMissingImage: missingImage)
+
+		//textView.delegate = self
+		textView.textAttachmentDelegate = self
+
+		attachProviders(textView)
+
 		return textView;
 	}
-	
+
+	func attachProviders(_ textView: Aztec.TextView) {
+		let font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
+		let providers: [TextViewAttachmentImageProvider] = [
+//			MoreAttachmentRenderer(),
+			CommentAttachmentRenderer(font: font),
+			HTMLAttachmentRenderer(font: font)
+		]
+
+		for provider in providers {
+			textView.registerAttachmentImageProvider(provider)
+		}
+	}
+
 	@objc func setContent(json: NSString, view: Aztec.TextView, defaultView: Aztec.TextView) {
 		let content = RCTConvert.nsString(json)
 		view.setHTML(content!)
 	}
+}
 
+extension EditorManager: UITextViewDelegate {
+
+}
+
+extension EditorManager: TextViewAttachmentDelegate {
+	func textView(_ textView: TextView, imageAt url: URL, onSuccess success: @escaping (UIImage) -> Void, onFailure failure: @escaping (Void) -> Void) -> UIImage {
+		// Todo: start task
+		return Gridicon.iconOfType(.image)
+	}
+
+	func textView(_ textView: TextView, urlFor imageAttachment: ImageAttachment) -> URL {
+		// TODO: start upload process
+		return URL(string: "placeholder://")!
+	}
+
+	func textView(_ textView: TextView, deletedAttachmentWith attachmentID: String) {
+		print("Attachment \(attachmentID) removed.\n")
+	}
+
+	func textView(_ textView: TextView, selected attachment: NSTextAttachment, atPosition position: CGPoint) {
+		print("Selected!")
+	}
+
+	func textView(_ textView: TextView, deselected attachment: NSTextAttachment, atPosition position: CGPoint) {
+		print("Deselected!")
+	}
 }
