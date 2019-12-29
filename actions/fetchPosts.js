fb5f7385ec0c16a926e1a6b94b4532cc1b55bf09
Joe Hoyle <joehoyle@gmail.com>
2019-12-29 14:13:17 +0100

    Formatting

--- actions/fetchPosts.js
+++ actions/fetchPosts.js
@@ -18,23 +18,24 @@ export default function fetchPosts( args ) {
 		const site = store.sites[store.activeSite.id];
 		const api = new httpapi( site );
 		let url = site.data.types[args.type]._links['wp:items'][0].href;
-		api.get( url, args ).then( data => {
-			dispatch( {
-				type: 'TYPE_POSTS_UPDATED',
-				payload: {
-					type: args.type,
-					posts: data,
-				},
+		api.get( url, args )
+			.then( data => {
+				dispatch( {
+					type: 'TYPE_POSTS_UPDATED',
+					payload: {
+						type: args.type,
+						posts: data,
+					},
+				} );
+			} )
+			.catch( error => {
+				dispatch( {
+					type: 'TYPES_POSTS_POST_UPDATE_ERRORED',
+					payload: {
+						type: args.type,
+						error,
+					},
+				} );
 			} );
-		} )
-		.catch( error => {
-			dispatch( {
-				type: 'TYPES_POSTS_POST_UPDATE_ERRORED',
-				payload: {
-					type: args.type,
-					error,
-				},
-			} );
-		})
 	};
 }
