d803e5c524b6df0f9d992f8734151d076d2c4c20
Ryan McCue <me@ryanmccue.info>
2020-01-05 21:55:45 +0000

    Use pkgutil to extract .pkg

--- get-sf-symbols.sh
+++ get-sf-symbols.sh
@@ -6,9 +6,11 @@ curl https://developer.apple.com/design/downloads/SF-Symbols.dmg > /tmp/sfsymbol
 # Mount the DMG
 echo "Extracting TTF…"
 hdiutil attach -noverify -nobrowse -mountpoint /tmp/sfsymbols /tmp/sfsymbols.dmg
-tar -O -xzf "/tmp/sfsymbols/SF Symbols.pkg" "SFSymbols.pkg/Payload" | tar -O -xz "./Applications/SF Symbols.app/Contents/Resources/SFSymbolsFallback.ttf" > images/SFSymbolsFallback.ttf
+pkgutil --expand "/tmp/sfsymbols/SF Symbols.pkg" /tmp/sfsymbols-extracted
+tar -O -f "/tmp/sfsymbols-extracted/SFSymbols.pkg/Payload" -xz "./Applications/SF Symbols.app/Contents/Resources/SFSymbolsFallback.ttf" > images/SFSymbolsFallback.ttf
 
 # Clean up.
 echo "Cleaning up…"
+rm -r /tmp/sfsymbols-extracted
 hdiutil detach /tmp/sfsymbols
 rm /tmp/sfsymbols.dmg
